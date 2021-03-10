3d20b5e3140e7d1c88a09fe9ef6dadc07de6ef18
jose <macaverde604@gmail.com>
2021-03-09 23:40:29 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,30 +1,16 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = getResultPrologue(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += getRentalFigures(each);
-      }
-      //add footer lines
-      getResultEpilogue(aCustomer);
-      return result;
-   }
-
-  private String getResultPrologue(Customer aCustomer) {
+  protected String getResultPrologue(Customer aCustomer) {
     return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
   }
 
-
-  private String getRentalFigures(Rental aRental) {
+  protected String getRentalFigures(Rental aRental) {
     return aRental.getMovie().getTitle()+ ": " +
            String.valueOf(aRental.getCharge()) + "<BR>\n";
   }
 
-  private String getResultEpilogue(Customer aCustomer) {
+  protected String getResultEpilogue(Customer aCustomer) {
     return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" +
           "On this rental you earned <EM>" +
           String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
--- Statement.java
+++ Statement.java
@@ -1,3 +1,21 @@
 public class Statement {
+  protected abstract String getResultPrologue(Customer aCustomer);
+  protected abstract String getRentalFigures(Rental aRental);
+  protected abstract String getResultEpilogue(Customer aCustomer);
 
+  public String value(Customer aCustomer) {
+    Enumeration rentals = aCustomer.getRentals();
+    String result = getResultPrologue(aCustomer);
+
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+
+      // show figures for this rental
+      result += getRentalFigures(each);
+    }
+
+    // add footer lines
+    result += getResultEpilogue(aCustomer);
+    return result;
+  }
 }
